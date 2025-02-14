1bedd109088019962d6aff2c9b8864176081ca51
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2025-02-14 11:11:14 +0100

    Modificando estilo

--- components/Routes/RouteForm.jsx
+++ components/Routes/RouteForm.jsx
@@ -8,6 +8,7 @@ import {
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
+import "./RouteForm.css";
 
 function RouteForm() {
   const { city } = useParams();
@@ -113,148 +114,165 @@ function RouteForm() {
   return (
     <div
       style={{
-        marginTop: "80px",
-        padding: "20px",
         display: "flex",
-        gap: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        margin: "0",
+        padding: "20px",
       }}
     >
       <div
         style={{
-          backgroundColor: "#f9f9f9",
-          padding: "30px",
-          borderRadius: "8px",
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-          flex: "1",
-          fontSize: "16px",
+          marginTop: "80px",
+          padding: "20px",
+          display: "flex",
+          gap: "20px",
+          maxWidth: "1200px",
+          width: "100%",
+          justifyContent: "center",
+          flexDirection: "column",
         }}
       >
-        <h2>{routeInfo.name}</h2>
-        <p>
-          <strong>Descripción:</strong> {routeInfo.description}
-        </p>
-        <p>
-          <strong>Duración estimada:</strong> {routeInfo.duration} minutos
-        </p>
-        <p>
-          <strong>Distancia total:</strong> {routeInfo.distance} km
-        </p>
-      </div>
-
-      <div style={{ display: "flex", flex: "3", gap: "20px" }}>
-        <MapContainer
-          center={coordinates}
-          zoom={15}
-          className="map"
-          style={{ width: "75%", height: "500px" }}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-          {checkpoints.map((checkpoint, index) => (
-            <Marker
-              key={index}
-              position={[
-                checkpoint.coordinates.startLatitude,
-                checkpoint.coordinates.startLongitude,
-              ]}
-            >
-              <Popup>
-                <div>
-                  <strong>{checkpoint.nameCheckpoint}</strong>
-                  <br />
-                  {checkpoint.audioguiaDTO?.url_audioguia.includes(".mp3") ? (
-                    <audio controls>
-                      <source
-                        src={checkpoint.audioguiaDTO.url_audioguia}
-                        type="audio/mpeg"
-                      />
-                      Tu navegador no soporta el elemento de audio.
-                    </audio>
-                  ) : (
-                    <a
-                      href={checkpoint.audioguiaDTO?.url_audioguia}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Audioguía
-                    </a>
-                  )}
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-
-          {route.length > 0 && (
-            <Polyline
-              positions={route.map((coord) => [coord[1], coord[0]])}
-              color="red"
-              weight={4}
-              dashArray="5, 10"
-            />
-          )}
-        </MapContainer>
-
         <div
           style={{
-            width: "25%",
-            backgroundColor: "#f1f1f1",
-            padding: "15px",
+            backgroundColor: "#f9f9f9",
+            padding: "30px",
             borderRadius: "8px",
-            boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+            flex: "1",
+            fontSize: "16px",
+            marginBottom: "20px",
+            textAlign: "center",
           }}
         >
-          <h3>Comentarios</h3>
+          <h2>{routeInfo.name}</h2>
+          <p>
+            <strong>Descripción:</strong> {routeInfo.description}
+          </p>
+          <p>
+            <strong>Duración estimada:</strong> {routeInfo.duration} minutos
+          </p>
+          <p>
+            <strong>Distancia total:</strong> {routeInfo.distance} km
+          </p>
+        </div>
+
+        <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+          <MapContainer
+            center={coordinates}
+            zoom={15}
+            className="map"
+            style={{ width: "75%", height: "500px" }}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+            {checkpoints.map((checkpoint, index) => (
+              <Marker
+                key={index}
+                position={[
+                  checkpoint.coordinates.startLatitude,
+                  checkpoint.coordinates.startLongitude,
+                ]}
+              >
+                <Popup>
+                  <div>
+                    <strong>{checkpoint.nameCheckpoint}</strong>
+                    <br />
+                    {checkpoint.audioguiaDTO?.url_audioguia.includes(".mp3") ? (
+                      <audio controls>
+                        <source
+                          src={checkpoint.audioguiaDTO.url_audioguia}
+                          type="audio/mpeg"
+                        />
+                        Tu navegador no soporta el elemento de audio.
+                      </audio>
+                    ) : (
+                      <a
+                        href={checkpoint.audioguiaDTO?.url_audioguia}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Audioguía
+                      </a>
+                    )}
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+
+            {route.length > 0 && (
+              <Polyline
+                positions={route.map((coord) => [coord[1], coord[0]])}
+                color="red"
+                weight={4}
+                dashArray="5, 10"
+              />
+            )}
+          </MapContainer>
+
           <div
             style={{
-              maxHeight: "400px",
-              overflowY: "auto",
-              marginBottom: "10px",
+              width: "25%",
+              backgroundColor: "#f1f1f1",
+              padding: "15px",
+              borderRadius: "8px",
+              boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
             }}
           >
-            {comments.length > 0 ? (
-              comments.map((comment, index) => (
-                <p
-                  key={index}
-                  style={{
-                    padding: "5px",
-                    borderBottom: "1px solid #ccc",
-                    fontSize: "14px",
-                  }}
-                >
-                  {comment.description}
-                </p>
-              ))
-            ) : (
-              <p>No hay comentarios aún.</p>
-            )}
+            <h3>Comentarios</h3>
+            <div
+              style={{
+                maxHeight: "400px",
+                overflowY: "auto",
+                marginBottom: "10px",
+              }}
+            >
+              {comments.length > 0 ? (
+                comments.map((comment, index) => (
+                  <p
+                    key={index}
+                    style={{
+                      padding: "5px",
+                      borderBottom: "1px solid #ccc",
+                      fontSize: "14px",
+                    }}
+                  >
+                    {comment.description}
+                  </p>
+                ))
+              ) : (
+                <p>No hay comentarios aún.</p>
+              )}
+            </div>
+            <input
+              type="text"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Añadir un comentario..."
+              style={{
+                width: "100%",
+                padding: "5px",
+                marginBottom: "5px",
+                fontSize: "13px",
+              }}
+            />
+            <button
+              onClick={handleAddComment}
+              style={{
+                width: "100%",
+                padding: "5px",
+                backgroundColor: "#007bff",
+                color: "white",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+                fontSize: "13px",
+              }}
+            >
+              Agregar
+            </button>
           </div>
-          <input
-            type="text"
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-            placeholder="Añadir un comentario..."
-            style={{
-              width: "100%",
-              padding: "5px",
-              marginBottom: "5px",
-              fontSize: "13px",
-            }}
-          />
-          <button
-            onClick={handleAddComment}
-            style={{
-              width: "100%",
-              padding: "5px",
-              backgroundColor: "#007bff",
-              color: "white",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              fontSize: "13px",
-            }}
-          >
-            Agregar
-          </button>
         </div>
       </div>
     </div>
